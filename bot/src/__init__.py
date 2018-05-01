7fea6b2c76a180c5e4c145a7fa6c83ae3e7af7bc
dmitrijbozhkov <dmitrijbozhkov@gmail.com>
2018-05-01 20:16:48 +0300

    deleted bot

--- bot/src/__init__.py
+++
@@ -1,7 +0,0 @@
-""" Web scraper for getting medical data """
-from run import run
-import sys
-
-if __name__ == "__main__":
-    print("pepe")
-    print(sys.path)
---
+++ ontology/__init__.py
@@ -0,0 +1,1 @@
+""" Developing ontology from mdical data """
